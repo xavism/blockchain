49700641536a5777d6980a23b237958eb8055356
xavism <xavi.sanchez.mir@gmail.com>
2021-01-04 23:44:57 +0100

    cli vue start

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
-import Blockchain from "./blockchain"
-import Transaction from "./transaction"
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-const EC = require('elliptic').ec
-const ec = new EC('secp256k1')
+Vue.config.productionTip = false;
 
-const myKey = ec.keyFromPrivate('4920dd825c58d65b8c72c6655c0e3de8841f7c3d42590f147b71f0cb8dcaa701')
-const myWalletAddress = myKey.getPublic('hex')
- 
-let xaviCoin = new Blockchain()
-console.log('Is chain Valid?', xaviCoin.isChainValid())
-
-const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10)
-tx1.signTransaction(myKey)
-xaviCoin.addTransaction(tx1)
-
-console.log('\nStarting the miner.⛏️..')
-xaviCoin.minePendingTransactions(myWalletAddress)
-
-console.log(`\nBalance of ${myWalletAddress} (the miner⛏️) is ${xaviCoin.getBalanceOfAddress(myWalletAddress)}`)
-console.log('Is chain Valid?', xaviCoin.isChainValid())
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
