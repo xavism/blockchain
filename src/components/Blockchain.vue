f3977958949ffa2f384d628fa5218492ef39ed4f
xavism <xavi.sanchez.mir@gmail.com>
2021-01-06 02:37:46 +0100

    tailwind nearly configured

---
+++ src/components/Blockchain.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>My Blochain</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Blockchain"
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Blockchain />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "Home",
   components: {
-    HelloWorld
+    Blockchain: () => import("@/components/Blockchain.vue")
   }
 });
 </script>
