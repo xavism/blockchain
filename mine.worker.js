dcaf50f56bb7eda1e622d39c8f504edb5cdd3e0b
xavism <xavi.sanchez.mir@gmail.com>
2021-03-16 03:14:57 +0100

    Updates

--- mine.worker.js
+++ mine.worker.js
@@ -1,44 +1,16 @@
-import { SHA256 } from 'crypto-js'
-//import ChainHelper from './../helpers/chain.helper'
-// const worker = () => {
-//   onmessage = (e) => {
-//     const data = e.data
-//     console.log('Worker: Message received from main script');
-//     const ChainHelper = require('./../helpers/chain.helper')
-//     let block = ChainHelper.mine(data.latestBlockHash, data.pendingTx, data.miningReward, data.difficulty, data.miningRewardAddress)
-//     postMessage(block);
-//   }
-// }
-  // eslint-disable-next-line no-restricted-globals
-// onmessage = function (e) {
-//   const data = e.data
-//   console.log('new Worker: Message received from main script')
-//   //let block = ChainHelper.mine(data.latestBlockHash, data.pendingTx, data.miningReward, data.difficulty, data.miningRewardAddress)
-//   postMessage('ei');
-// }
-
-
-// export default worker
-// onmessage = (e) => {
-//     const data = e.data
-//     console.log('Worker: Message received from main script');
-//     let block = ChainHelper.mine(data.latestBlockHash, data.pendingTx, data.miningReward, data.difficulty, data.miningRewardAddress)
-//     postMessage(block);
-// };
-export default onmessage = (e) => {
+onmessage = async (e) => {
   const data = e.data
-  console.log('Worker: Message received from main script');
-  let block = mine(data.latestBlockHash, data.pendingTx, data.miningReward, data.difficulty, data.miningRewardAddress)
+  let block = await mine(data.latestBlockHash, data.pendingTx, data.miningReward, data.difficulty, data.miner)
   postMessage(block);
 }
 
-const mine = (latestBlockHash, pendingTx, miningReward, difficulty, minerAddress) => {
+const mine = async (latestBlockHash, pendingTx, miningReward, difficulty, miner) => {
   // Adding the miner Tx to the pending ones to get the reward
-  const rewardTX = createTx(null, minerAddress, miningReward)
+  const rewardTX = createTx(null, miner, miningReward)
   pendingTx.push(rewardTX)
   // Creating a block containing the pending transactions
-  const block = createBlock(new Date().toLocaleDateString(), pendingTx, latestBlockHash)
-  mineBlock(block, difficulty)
+  const block = await createBlock(new Date().toISOString().split('T')[0], pendingTx, latestBlockHash)
+  await mineBlock(block, difficulty)
   return block
 }
 
@@ -52,24 +24,38 @@ const createTx = (from, to, amount) => {
   }
 }
 
-const createBlock = (timestamp, transactions, previousHash = '') => {
+const createBlock = async (timestamp, transactions, previousHash = '') => {
   return {
+    id: Math.random().toString(36).substr(2, 9),
     timestamp,
     transactions,
     previousHash,
     nonce: 0,
-    hash: calculateHash(timestamp, transactions, previousHash, 0)
+    hash: await calculateHash(timestamp, transactions, previousHash, 0)
   }
 }
 
-const mineBlock = (block, difficulty) => {
+const mineBlock = async (block, difficulty) => {
   let { timestamp, transactions, previousHash } = block
   while (block.hash.substring(0, difficulty) !== Array(difficulty).fill(0).join('')) {
+    if (block.nonce % 10000 === 0) console.log(block.nonce)
     block.nonce++
-    block.hash = calculateHash(timestamp, transactions, previousHash, block.nonce)
+    block.hash = await calculateHash(timestamp, transactions, previousHash, block.nonce)
   }
 }
 
-const calculateHash = (timestamp, transactions, previousHash, nonce) => {
-  return SHA256(timestamp + JSON.stringify(transactions) + previousHash + nonce).toString()
+const calculateHash = async (timestamp, transactions, previousHash, nonce) => {
+  return await sha256(timestamp + JSON.stringify(transactions) + previousHash + nonce)
+} 
+
+async function sha256(message) {
+  // encode as UTF-8
+  const msgBuffer = new TextEncoder('utf-8').encode(message);
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  // convert ArrayBuffer to Array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  // convert bytes to hex string
+  const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+  return hashHex;
 }
